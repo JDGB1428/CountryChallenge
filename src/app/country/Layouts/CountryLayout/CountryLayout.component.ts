c10a095e50bd944bc8983e4873b4d4766074fe9d
JDGB1428 <juangalindo28@hotmail.com>
2025-05-08 23:57:02 -0500

    Eliminar NavbarComponent de CountryLayout y ajustar las importaciones

--- src/app/country/Layouts/CountryLayout/CountryLayout.component.ts
+++ src/app/country/Layouts/CountryLayout/CountryLayout.component.ts
@@ -1,11 +1,10 @@
-import { Component, effect, HostBinding, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterLink,RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-country-layout',
-  imports: [RouterOutlet, RouterLink, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './CountryLayout.component.html',
 })
 export class CountryLayoutComponent {
