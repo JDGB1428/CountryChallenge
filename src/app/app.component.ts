4a5576c0653d02ec3791d1772c5d246b63b4c7db
JDGB1428 <juangalindo28@hotmail.com>
2025-05-08 23:56:48 -0500

    Agregar NavbarComponent y DarkModeService al componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { DarkModeService } from './country/services/dark-mode.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'CountryChallenge';
+
+  darkModeService:DarkModeService = inject(DarkModeService)
 }
