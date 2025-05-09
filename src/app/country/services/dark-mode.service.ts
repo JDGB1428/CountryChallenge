65f0a69995bf32dedb369e66f0d6045cd099ed71
JDGB1428 <juangalindo28@hotmail.com>
2025-05-09 00:00:50 -0500

    Actualizar el servicio DarkMode para inicializar el estado desde localStorage y optimizar la gestión del modo oscuro

--- src/app/country/services/dark-mode.service.ts
+++ src/app/country/services/dark-mode.service.ts
@@ -1,14 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarkModeService {
 
-  darkModeSignal = signal<string>('null')
+  darkModeSignal = signal<string>(
+    JSON.parse(window.localStorage.getItem('darkMode') ?? 'null')
+  )
 
   updateDarkMode(){
     this.darkModeSignal.update((prev) => (prev === 'dark' ? 'null' : 'dark'))
+  }
+
+  constructor(){
+    effect(() => {
+      window.localStorage.setItem('darkMode', JSON.stringify(this.darkModeSignal()))
+    })
   }
 
 }
